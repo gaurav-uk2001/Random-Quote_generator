aa328b7740b4faaf3f686f464364bcc8699830d6
gaurav-uk2001 <gaurav.vaishali2001@gmail.com>
2022-07-17 23:37:07 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-console.log('testing my first project');
+// should match with id with respective id in HTML
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];  
+
+
+// show new Quote
+function newQuote(){
+    //pick a random Quote from the apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
+    
+    // check if Author field is blank and replace with "Unkown"
+    if(!quote.author)
+    {
+        authorText.textContent = 'Unkown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+
+    // check Quote length to determine the styling
+    if(quote.text.length > 120)
+    {
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
+}
+
+
+// Get Quotes form the API
+async function getQuotes(){
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch(error){
+        //Catch error here
+        alert(error);
+    }
+}
+
+// Tweet Quote using query parameters
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}-${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on Load
+getQuotes();
